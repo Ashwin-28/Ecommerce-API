8ce88c3e1755014ed892e77caecaddda68d80796
zorobin04 <nggopinath04@gmail.com>
2026-04-16 10:09:52 +0530

    auditlog service added

--- Ecommerce-API/Program.cs
+++ Ecommerce-API/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<AdminUserService>();
+builder.Services.AddScoped<AuditLogService>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
