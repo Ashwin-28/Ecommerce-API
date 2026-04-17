44645a23ab1307d5dbf85cb04e636e2488a47dc8
zorobin04 <nggopinath04@gmail.com>
2026-04-17 08:22:16 +0530

    small changes in program.cs

--- Ecommerce-API/Program.cs
+++ Ecommerce-API/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped<AdminUserService>();
 builder.Services.AddScoped<AuditLogService>();
+builder.Services.AddScoped<AuthServices>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
